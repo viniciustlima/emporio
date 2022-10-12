4a34923158a94904b51334f32d862eff2b7e041e
viniciustlima <vinitlima2007.14@gmail.com>
2022-10-12 14:44:55 -0300

    edit user credentials

--- src/app/controllers/user.controller.js
+++ src/app/controllers/user.controller.js
@@ -15,6 +15,36 @@ class UserController {
 	}
 
 	editInfoP = (req, res) => {
+		const { name, username, password } = req.body
+		var login = req.session.login
+
+		try {
+			var data = JSON.parse(fs.readFileSync(
+				path.join(__dirname, '../config/users.json'),
+				'utf8'
+			))
+
+			var users = data.users
+
+			for (let i = 0; i < users.length; i++) {
+				if (users[i].id == login.id) {
+					if (users[i].name != name) users[i].name = name
+					if (users[i].username != username) users[i].username = username
+					if (users[i].password != password) users[i].password = password
+
+					fs.writeFileSync(
+						path.join(__dirname, '../config/users.json'),
+						JSON.stringify(data),
+						'utf8'
+					)
+
+					return res.redirect('/admin/logout')
+				}
+			}
+		} catch (e) {
+			return res.redirect('/admin')
+		}
+
 		return res.send('ok')
 	}
 }
